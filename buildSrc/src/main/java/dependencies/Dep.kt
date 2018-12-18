35788ec8f083b3f4a6cc18b6139e9b6213bbb788
takahirom <takam.dev@gmail.com>
2018-12-18 10:43:49 +0900

    Refactor dependencies

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -1,30 +1,24 @@
 package dependencies
 
 object Dep {
-    object Kotlin {
-        val version = "1.3.11"
-        val plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
-        val serializationPlugin = "org.jetbrains.kotlin:kotlin-serialization:$version"
-        val stdlibCommon = "org.jetbrains.kotlin:kotlin-stdlib-common:$version"
-        val stdlibJvm = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
-        val coroutinesVersion = "1.0.1"
-        val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
-        val androidCoroutinesDispatcher = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion"
-        val coroutinesRx2 = "org.jetbrains.kotlinx:kotlinx-coroutines-rx2:$coroutinesVersion"
-        val coroutinesReactive = "org.jetbrains.kotlinx:kotlinx-coroutines-reactive:$coroutinesVersion"
-        val androidCoroutines = "net.devrieze:android-coroutines:0.7.0"
-        val coroutinesPlayServices = "org.jetbrains.kotlinx:kotlinx-coroutines-play-services:$coroutinesVersion"
-        val serialization = "org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.8.1-rc13"
-        val jvmModuleTest = "org.jetbrains.kotlin:kotlin-test"
-        val jvmModuleTestJunit = "org.jetbrains.kotlin:kotlin-test-junit"
-        val commonModuleTest = "org.jetbrains.kotlin:kotlin-test-common"
-        val commonModuleTestAnnotations = "org.jetbrains.kotlin:kotlin-test-annotations-common"
+    object GradlePlugin {
+        val android = "com.android.tools.build:gradle:3.2.0"
+        val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
+        val kotlinSerialization = "org.jetbrains.kotlin:kotlin-serialization:${Kotlin.version}"
+        val playServices = "com.google.gms:google-services:4.1.0"
+        val safeArgs = "android.arch.navigation:navigation-safe-args-gradle-plugin:${AndroidX.Navigation.version}"
     }
 
-    object RxJava {
-    }
+    object AndroidX {
+        val appCompat = "androidx.appcompat:appcompat:1.0.0"
+        val recyclerView = "androidx.recyclerview:recyclerview:1.0.0"
+        val constraint = "androidx.constraintlayout:constraintlayout:1.1.2"
+        val testRunner = "androidx.test:runner:1.1.0-alpha3"
+        val emoji = "androidx.emoji:emoji-appcompat:1.0.0"
+        val espressoCore = "androidx.test.espresso:espresso-core:3.1.0-alpha3"
+        val design = "com.google.android.material:material:1.1.0-alpha01"
+        val coreKtx = "androidx.core:core-ktx:1.0.0-alpha1"
 
-    object Arch {
         val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:2.0.0"
         val lifecycleLiveData = "androidx.lifecycle:lifecycle-livedata:2.0.0"
         val coreTesting = "androidx.arch.core:core-testing:2.0.0"
@@ -44,9 +38,26 @@ object Dep {
             val ui = "android.arch.navigation:navigation-ui:$version"
             val fragmentKtx = "android.arch.navigation:navigation-fragment-ktx:$version"
             val uiKtx = "android.arch.navigation:navigation-ui-ktx:$version"
-            val safeArgsPlugin = "android.arch.navigation:navigation-safe-args-gradle-plugin:$version"
             val testingKtx = "android.arch.navigation:navigation-testing-ktx:$version"
         }
+    }
+
+    object Kotlin {
+        val version = "1.3.11"
+        val stdlibCommon = "org.jetbrains.kotlin:kotlin-stdlib-common:$version"
+        val stdlibJvm = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
+        val coroutinesVersion = "1.0.1"
+        val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
+        val androidCoroutinesDispatcher = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion"
+        val coroutinesRx2 = "org.jetbrains.kotlinx:kotlinx-coroutines-rx2:$coroutinesVersion"
+        val coroutinesReactive = "org.jetbrains.kotlinx:kotlinx-coroutines-reactive:$coroutinesVersion"
+        val androidCoroutines = "net.devrieze:android-coroutines:0.7.0"
+        val coroutinesPlayServices = "org.jetbrains.kotlinx:kotlinx-coroutines-play-services:$coroutinesVersion"
+        val serialization = "org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.8.1-rc13"
+        val jvmModuleTest = "org.jetbrains.kotlin:kotlin-test"
+        val jvmModuleTestJunit = "org.jetbrains.kotlin:kotlin-test-junit"
+        val commonModuleTest = "org.jetbrains.kotlin:kotlin-test-common"
+        val commonModuleTestAnnotations = "org.jetbrains.kotlin:kotlin-test-annotations-common"
     }
 
     object Firebase {
@@ -56,7 +67,6 @@ object Dep {
     }
 
     object PlayServices {
-        val plugin = "com.google.gms:google-services:4.1.0"
         val auth = "com.google.android.gms:play-services-auth:16.0.1"
     }
 
@@ -71,36 +81,25 @@ object Dep {
         val assistedInjectProcessor = "com.squareup.inject:assisted-inject-processor-dagger2:0.3.0"
     }
 
-    object Android {
-        val gradlePlugin = "com.android.tools.build:gradle:3.2.0"
-        val appCompat = "androidx.appcompat:appcompat:1.0.0"
-        val recyclerView = "androidx.recyclerview:recyclerview:1.0.0"
-        val constraint = "androidx.constraintlayout:constraintlayout:1.1.2"
-        val testRunner = "androidx.test:runner:1.1.0-alpha3"
-        val emoji = "androidx.emoji:emoji-appcompat:1.0.0"
-        val espressoCore = "androidx.test.espresso:espresso-core:3.1.0-alpha3"
-        val design = "com.google.android.material:material:1.1.0-alpha01"
-    }
-
     object Ktor {
-        val clientAndroid = "io.ktor:ktor-client-android:1.0.1"
-        val jsonJvm = "io.ktor:ktor-client-json-jvm:1.0.1"
-    }
-
-    object AndroidKtx {
-        val core = "androidx.core:core-ktx:1.0.0-alpha1"
+        val version = "1.0.1"
+        val clientAndroid = "io.ktor:ktor-client-okhttp:$version"
+        val jsonJvm = "io.ktor:ktor-client-json-jvm:$version"
     }
 
     object OkHttp {
-        val client = "com.squareup.okhttp3:okhttp:3.11.0"
+        val version = "3.11.0"
+        val client = "com.squareup.okhttp3:okhttp:$version"
+        val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$version"
     }
 
     val liveDataKtx = "com.shopify:livedata-ktx:2.0.1"
 
     object LeakCanary {
-        val leakCanary = "com.squareup.leakcanary:leakcanary-android:1.6.2"
-        val leakCanaryNoOp = "com.squareup.leakcanary:leakcanary-android-no-op:1.6.2"
-        val leakCanaryFragment = "com.squareup.leakcanary:leakcanary-support-fragment:1.6.2"
+        val version = "1.6.2"
+        val leakCanary = "com.squareup.leakcanary:leakcanary-android:$version"
+        val leakCanaryNoOp = "com.squareup.leakcanary:leakcanary-android-no-op:$version"
+        val leakCanaryFragment = "com.squareup.leakcanary:leakcanary-support-fragment:$version"
     }
 
     object Stetho {
@@ -108,28 +107,31 @@ object Dep {
     }
 
     object Hyperion {
+        val version = "0.9.24"
         val hyperionPlugins = listOf(
-            "com.willowtreeapps.hyperion:hyperion-core:0.9.24",
-            "com.willowtreeapps.hyperion:hyperion-attr:0.9.24",
-            "com.willowtreeapps.hyperion:hyperion-measurement:0.9.24",
-            "com.willowtreeapps.hyperion:hyperion-disk:0.9.24",
-            "com.willowtreeapps.hyperion:hyperion-recorder:0.9.24",
-            "com.willowtreeapps.hyperion:hyperion-phoenix:0.9.24",
-            "com.willowtreeapps.hyperion:hyperion-crash:0.9.24",
-            "com.willowtreeapps.hyperion:hyperion-shared-preferences:0.9.24",
-            "com.willowtreeapps.hyperion:hyperion-geiger-counter:0.9.24",
-            "com.willowtreeapps.hyperion:hyperion-build-config:0.9.24"
+            "com.willowtreeapps.hyperion:hyperion-core:$version",
+            "com.willowtreeapps.hyperion:hyperion-attr:$version",
+            "com.willowtreeapps.hyperion:hyperion-measurement:$version",
+            "com.willowtreeapps.hyperion:hyperion-disk:$version",
+            "com.willowtreeapps.hyperion:hyperion-recorder:$version",
+            "com.willowtreeapps.hyperion:hyperion-phoenix:$version",
+            "com.willowtreeapps.hyperion:hyperion-crash:$version",
+            "com.willowtreeapps.hyperion:hyperion-shared-preferences:$version",
+            "com.willowtreeapps.hyperion:hyperion-geiger-counter:$version",
+            "com.willowtreeapps.hyperion:hyperion-build-config:$version"
         )
     }
 
     object Groupie {
-        val groupie = "com.xwray:groupie:2.1.0"
-        val databinding = "com.xwray:groupie-databinding:2.1.0"
+        val version = "2.1.0"
+        val groupie = "com.xwray:groupie:$version"
+        val databinding = "com.xwray:groupie-databinding:$version"
     }
 
     object Klock {
-        val common = "com.soywiz:klock:1.0.0"
-        val jvm = "com.soywiz:klock-jvm:1.0.0"
+        val version = "1.0.0"
+        val common = "com.soywiz:klock:$version"
+        val jvm = "com.soywiz:klock-jvm:$version"
     }
 
     object KotlinLogging {
@@ -148,9 +150,10 @@ object Dep {
     }
 
     object InjectedVmProvider {
-        val injectedVmProvider = "me.tatarka.injectedvmprovider:injectedvmprovider:2.0"
-        val extension = "me.tatarka.injectedvmprovider:injectedvmprovider-extensions:2.0"
-        val ktx = "me.tatarka.injectedvmprovider:injectedvmprovider-ktx:2.0"
+        val version = 2.0
+        val injectedVmProvider = "me.tatarka.injectedvmprovider:injectedvmprovider:$version"
+        val extension = "me.tatarka.injectedvmprovider:injectedvmprovider-extensions:$version"
+        val ktx = "me.tatarka.injectedvmprovider:injectedvmprovider-ktx:$version"
     }
 
     object Picasso {
