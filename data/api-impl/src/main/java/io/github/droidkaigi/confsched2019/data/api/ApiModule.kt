6d2585016217ec708ee0a3a1da4342bbecd96389
takahirom <takam.dev@gmail.com>
2018-12-19 10:24:10 +0900

    Add dispatcher class for testing

--- data/api-impl/src/main/java/io/github/droidkaigi/confsched2019/data/api/ApiModule.kt
+++ data/api-impl/src/main/java/io/github/droidkaigi/confsched2019/data/api/ApiModule.kt
@@ -3,10 +3,13 @@ package io.github.droidkaigi.confsched2019.data.api
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import io.github.droidkaigi.confsched2019.BuildConfig
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
 import kotlinx.serialization.json.JSON
+import okhttp3.logging.HttpLoggingInterceptor
 
 @Module(includes = [ApiModule.Providers::class])
 internal abstract class ApiModule {
@@ -15,7 +18,14 @@ internal abstract class ApiModule {
     @Module
     internal object Providers {
         @JvmStatic @Provides fun httpClient(): HttpClient {
-            return HttpClient {
+            return HttpClient(OkHttp) {
+                engine {
+                    if (BuildConfig.DEBUG) {
+                        val loggingInterceptor = HttpLoggingInterceptor()
+                        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+                        addInterceptor(loggingInterceptor)
+                    }
+                }
                 install(JsonFeature) {
                     serializer = KotlinxSerializer(JSON.nonstrict)
                 }
