b344e8713efad8d20d3ee48838489f808ea07da7
Tomoya Miwa <tomoyann@gmail.com>
2019-01-05 01:08:56 +0900

    Add support for session message

--- model/src/commonMain/kotlin/SessionMessage.kt
+++ model/src/commonMain/kotlin/SessionMessage.kt
@@ -1,3 +1,13 @@
 package io.github.droidkaigi.confsched2019.model
 
-data class SessionMessage(val jaMessage: String, val enMessage: String)
+data class SessionMessage(
+    val jaMessage: String,
+    val enMessage: String
+) {
+
+    fun getBodyByLang(lang: Lang): String = if (lang == Lang.JA) {
+        jaMessage
+    } else {
+        enMessage
+    }
+}
