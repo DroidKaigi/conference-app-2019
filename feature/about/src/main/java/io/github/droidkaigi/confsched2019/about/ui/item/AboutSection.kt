ba249422c93c8416a0cce9ffa2d39f6a11f05139
takahirom <takam.dev@gmail.com>
2019-01-07 00:32:50 +0900

    Show toast instead of crash

--- feature/about/src/main/java/io/github/droidkaigi/confsched2019/about/ui/item/AboutSection.kt
+++ feature/about/src/main/java/io/github/droidkaigi/confsched2019/about/ui/item/AboutSection.kt
@@ -1,5 +1,6 @@
 package io.github.droidkaigi.confsched2019.about.ui.item
 
+import android.widget.Toast
 import com.xwray.groupie.Section
 import io.github.droidkaigi.confsched2019.about.R
 
@@ -13,25 +14,25 @@ class AboutSection : Section() {
                     R.string.about_access_to_place,
                     R.string.about_check_map
                 ) {
-                    TODO()
+                    Toast.makeText(it, "FIXME!!", Toast.LENGTH_SHORT).show()
                 },
                 AboutItem(
                     R.string.about_staff_list,
                     R.string.about_check
                 ) {
-                    TODO()
+                    Toast.makeText(it, "FIXME!!", Toast.LENGTH_SHORT).show()
                 },
                 AboutItem(
                     R.string.about_privacy_policy,
                     R.string.about_check
                 ) {
-                    TODO()
+                    Toast.makeText(it, "FIXME!!", Toast.LENGTH_SHORT).show()
                 },
                 AboutItem(
                     R.string.about_license,
                     R.string.about_check
                 ) {
-                    TODO()
+                    Toast.makeText(it, "FIXME!!", Toast.LENGTH_SHORT).show()
                 },
                 AboutItem(
                     R.string.about_app_version,
