423187ad7c0338ad3e93352277cdba71f1116abe
SatoShun <shun.sato1@gmail.com>
2019-01-07 14:53:35 +0000

    repository commonMain: fix directory paht with to package

--- data/repository/src/commonMain/kotlin/io/github/droidkaigi/confsched2019/data/repository/SponsorRepository.kt
+++ data/repository/src/commonMain/kotlin/io/github/droidkaigi/confsched2019/data/repository/SponsorRepository.kt
@@ -1,8 +1,6 @@
 package io.github.droidkaigi.confsched2019.data.repository
 
-import io.github.droidkaigi.confsched2019.model.Sponsor
 import io.github.droidkaigi.confsched2019.model.SponsorCategory
-import kotlinx.coroutines.channels.ReceiveChannel
 
 interface SponsorRepository {
     suspend fun sponsors(): List<SponsorCategory>
