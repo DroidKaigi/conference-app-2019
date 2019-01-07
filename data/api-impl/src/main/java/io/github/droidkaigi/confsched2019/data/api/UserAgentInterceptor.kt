9e69113ede0110f244ee16a5319cc054f47638cb
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-01-07 21:47:30 +0900

    Formatted the builder syntax

--- data/api-impl/src/main/java/io/github/droidkaigi/confsched2019/data/api/UserAgentInterceptor.kt
+++ data/api-impl/src/main/java/io/github/droidkaigi/confsched2019/data/api/UserAgentInterceptor.kt
@@ -4,8 +4,10 @@ import io.github.droidkaigi.confsched2019.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
-internal class UserAgentInterceptor: Interceptor {
+internal class UserAgentInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        return chain.proceed(chain.request().newBuilder().addHeader("User-Agent", "official-app-2019/${BuildConfig.VERSION_CODE}").build())
+        return chain.proceed(chain.request().newBuilder().apply {
+            addHeader("User-Agent", "official-app-2019/${BuildConfig.VERSION_CODE}")
+        }.build())
     }
 }
