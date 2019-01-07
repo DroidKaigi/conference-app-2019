a09d067ba0889a2d84aad14aeb44439ab1ffe969
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-01-07 21:30:51 +0900

    Specify UserAgent

---
+++ data/api-impl/src/main/java/io/github/droidkaigi/confsched2019/data/api/UserAgentInterceptor.kt
@@ -0,0 +1,11 @@
+package io.github.droidkaigi.confsched2019.data.api
+
+import io.github.droidkaigi.confsched2019.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+
+internal class UserAgentInterceptor: Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        return chain.proceed(chain.request().newBuilder().addHeader("User-Agent", "official-app-2019/${BuildConfig.VERSION_CODE}").build())
+    }
+}
