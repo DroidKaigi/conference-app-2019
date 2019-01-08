f277f4038366a314e19f29726f0a39ba683451ba
kikuchy <kikuchy.h@gmail.com>
2019-01-08 19:18:15 +0900

    Shorten line length.

--- data/api-impl/src/main/java/io/github/droidkaigi/confsched2019/data/api/InjectableKtorGoogleFormApi.kt
+++ data/api-impl/src/main/java/io/github/droidkaigi/confsched2019/data/api/InjectableKtorGoogleFormApi.kt
@@ -3,4 +3,6 @@ package io.github.droidkaigi.confsched2019.data.api
 import io.ktor.client.HttpClient
 import javax.inject.Inject
 
-class InjectableKtorGoogleFormApi @Inject constructor(httpClient: HttpClient) : KtorGoogleFormApi(httpClient)
+class InjectableKtorGoogleFormApi @Inject constructor(
+    httpClient: HttpClient
+) : KtorGoogleFormApi(httpClient)
