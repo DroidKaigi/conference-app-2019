13c9833a07db7c3866580b015f3d491dd941872a
kazumax1218 <kazumax1218@gmail.com>
2019-01-09 18:19:28 +0900

    Remove unused import

--- frontend/android/src/main/java/io/github/droidkaigi/confsched2019/ui/MainActivity.kt
+++ frontend/android/src/main/java/io/github/droidkaigi/confsched2019/ui/MainActivity.kt
@@ -3,7 +3,6 @@ package io.github.droidkaigi.confsched2019.ui
 import android.graphics.PorterDuff
 import android.os.Build
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.core.content.ContextCompat
