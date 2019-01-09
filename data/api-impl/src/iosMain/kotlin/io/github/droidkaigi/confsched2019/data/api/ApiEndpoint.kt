41eefd0500927218163adfb4343c473335189ba6
kikuchy <kikuchy.h@gmail.com>
2019-01-09 16:18:33 +0900

    Make :api and :api-impl buildable for iOS Framework.

---
+++ data/api-impl/src/iosMain/kotlin/io/github/droidkaigi/confsched2019/data/api/ApiEndpoint.kt
@@ -0,0 +1,5 @@
+package io.github.droidkaigi.confsched2019.data.api
+
+// TODO: Replace with code getting endpoint from iOS application build configurations.
+//       This is temporally implementation.
+internal actual fun apiEndpoint(): String = "https://droidkaigi2019-dev.appspot.com/api"
