933a63a75b7d1ab20819efd5011007ce381bedfd
kikuchy <kikuchy.h@gmail.com>
2019-01-09 11:31:55 +0900

    Make :model buildable for iOS Framework

--- model/src/commonMain/kotlin/LocaleMessage.kt
+++ model/src/commonMain/kotlin/LocaleMessage.kt
@@ -2,4 +2,4 @@ package io.github.droidkaigi.confsched2019.model
 
 class LocaleMessage(val enMessage: String, val jaMessage: String)
 
-expect fun LocaleMessage.get(): String
+fun LocaleMessage.get() = if (defaultLang() != Lang.JA) enMessage else jaMessage
