6854f0f6de2122833c070b0f2fe1323d8c738936
furusin <furusin.oriver@gmail.com>
2019-01-09 12:22:01 +0900

    Rename FireStoreComponentModule.kt to FirestoreComponentModule.kt

--- frontend/android/src/main/java/io/github/droidkaigi/confsched2019/di/FireStoreComponentModule.kt
+++ frontend/android/src/main/java/io/github/droidkaigi/confsched2019/di/FireStoreComponentModule.kt
@@ -3,14 +3,14 @@ package io.github.droidkaigi.confsched2019.di
 import dagger.Module
 import dagger.Provides
 import io.github.droidkaigi.confsched2019.data.firestore.Firestore
-import io.github.droidkaigi.confsched2019.data.repository.FireStoreComponent
+import io.github.droidkaigi.confsched2019.data.repository.FirestoreComponent
 import io.github.droidkaigi.confsched2019.ext.android.Dispatchers
 import javax.inject.Singleton
 
 @Module
-object FireStoreComponentModule {
+object FirestoreComponentModule {
     @JvmStatic @Provides @Singleton fun provideRepository(): Firestore {
-        return FireStoreComponent.builder()
+        return FirestoreComponent.builder()
             .coroutineContext(Dispatchers.IO)
             .build()
             .firestore()
