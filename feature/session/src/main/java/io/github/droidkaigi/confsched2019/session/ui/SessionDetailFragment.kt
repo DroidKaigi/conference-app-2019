0e26efd809d7d633a88b1d379cca7a91e4843e09
h3birth <h3.birth@gmail.com>
2019-01-10 03:01:50 +0900

    add suppress StringFormatMatches

--- feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/SessionDetailFragment.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/SessionDetailFragment.kt
@@ -70,6 +70,7 @@ class SessionDetailFragment : DaggerFragment() {
             when (item.itemId) {
                 R.id.session_share -> {
                     val session = binding.session ?: return@setOnMenuItemClickListener false
+                    @Suppress("StringFormatMatches") // FIXME
                     activityActionCreator.shareUrl(getString(
                         R.string.session_detail_share_url,
                         session.id.toInt()
