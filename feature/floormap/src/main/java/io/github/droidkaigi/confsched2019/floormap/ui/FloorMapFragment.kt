5a9eda311f1b9724a096bcef366d513012a7e371
androhi <takahiro.1828@gmail.com>
2019-01-11 20:24:38 +0900

    Delete unnecessary class for FloorMap

--- feature/floormap/src/main/java/io/github/droidkaigi/confsched2019/floormap/ui/FloorMapFragment.kt
+++ feature/floormap/src/main/java/io/github/droidkaigi/confsched2019/floormap/ui/FloorMapFragment.kt
@@ -4,38 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager.widget.PagerAdapter
 import dagger.Module
 import dagger.Provides
 import io.github.droidkaigi.confsched2019.di.PageScope
-import io.github.droidkaigi.confsched2019.ext.android.changed
-import io.github.droidkaigi.confsched2019.model.FloorMap
 import io.github.droidkaigi.confsched2019.floormap.R
 import io.github.droidkaigi.confsched2019.floormap.databinding.FragmentFloorMapBinding
 import io.github.droidkaigi.confsched2019.floormap.databinding.ItemFloorMapBinding
-import io.github.droidkaigi.confsched2019.floormap.ui.actioncreator.FloorMapActionCreator
-import io.github.droidkaigi.confsched2019.floormap.ui.store.FloorMapStore
 import io.github.droidkaigi.confsched2019.floormap.ui.widget.DaggerFragment
-import io.github.droidkaigi.confsched2019.model.LoadingState
-import io.github.droidkaigi.confsched2019.util.ProgressTimeLatch
-import me.tatarka.injectedvmprovider.InjectedViewModelProviders
-import javax.inject.Inject
-import javax.inject.Provider
+import io.github.droidkaigi.confsched2019.model.FloorMap
 
 class FloorMapFragment : DaggerFragment() {
 
     private lateinit var binding: FragmentFloorMapBinding
-
-    @Inject lateinit var floorMapStoreProvider: Provider<FloorMapStore>
-    private val floorMapStore: FloorMapStore by lazy {
-        InjectedViewModelProviders.of(requireActivity())[floorMapStoreProvider]
-    }
-    @Inject lateinit var floorMapActionCreator: FloorMapActionCreator
-
-    private lateinit var progressTimeLatch: ProgressTimeLatch
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -53,18 +36,7 @@ class FloorMapFragment : DaggerFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
         setupFloorMapPager()
-
-        progressTimeLatch = ProgressTimeLatch { showProgress ->
-            binding.progressBar.isVisible = showProgress
-        }.apply {
-            loading = true
-        }
-        floorMapStore.loadingState.changed(viewLifecycleOwner) {
-            progressTimeLatch.loading = it == LoadingState.LOADING
-        }
-        floorMapActionCreator.load()
     }
 
     private fun setupFloorMapPager() {
