496fef8ed7e23ac2f0855760fe0637260cb27808
Yutaro Iino <ytrino@gmail.com>
2019-01-11 14:34:43 +0900

    Remove redundant namespace on BindingAdapter

--- feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/bindingadapter/ViewBinding.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/bindingadapter/ViewBinding.kt
@@ -3,7 +3,7 @@ package io.github.droidkaigi.confsched2019.session.ui.bindingadapter
 import android.view.View
 import androidx.databinding.BindingAdapter
 
-@BindingAdapter(value = ["app:visibleGone"])
+@BindingAdapter(value = ["visibleGone"])
 fun showHide(view: View, show: Boolean) {
     view.visibility = if (show) View.VISIBLE else View.GONE
 }
