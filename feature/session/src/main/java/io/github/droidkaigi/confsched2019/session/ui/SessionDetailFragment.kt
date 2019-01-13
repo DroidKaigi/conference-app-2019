ef3fe74cba350b9c9cb5d2c58484c5ef9cbee85d
momochaaam <kqss.251@gmail.com>
2019-01-14 01:19:24 +0900

    stringsに定義

--- feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/SessionDetailFragment.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/SessionDetailFragment.kt
@@ -125,7 +125,7 @@ class SessionDetailFragment : DaggerFragment() {
                 return@addOnDrawListener
             }
             val end = textView.layout.getLineStart(5)
-            val ellipsis = "…続きを読む"
+            val ellipsis = getString(R.string.ellipsis_label)
             val text = textView.text.subSequence(0, end-ellipsis.length).toString().plus(ellipsis)
             val spannable = SpannableString(text)
             spannable.setSpan(ForegroundColorSpan(Color.BLUE), text.lastIndex-ellipsis.length+1, text.lastIndex+1, Spannable.SPAN_COMPOSING)
