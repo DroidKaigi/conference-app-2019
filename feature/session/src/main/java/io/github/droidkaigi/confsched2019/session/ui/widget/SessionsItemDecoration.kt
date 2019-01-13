f49d74ef0ac580eefa2463f26797ccef0b348b49
Gen <archiherewego@gmail.com>
2019-01-14 00:13:59 +0900

    fix to work correctly under the filtering

--- feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/widget/SessionsItemDecoration.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/widget/SessionsItemDecoration.kt
@@ -10,9 +10,9 @@ import android.util.SparseArray
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
-import androidx.core.util.contains
 import androidx.core.util.forEach
 import androidx.recyclerview.widget.RecyclerView
+import com.soywiz.klock.DateTime
 import com.soywiz.klock.DateTimeSpan
 import com.xwray.groupie.GroupAdapter
 import io.github.droidkaigi.confsched2019.session.R
@@ -42,7 +42,7 @@ class SessionsItemDecoration(
     ).toFloat()
     // Keep SparseArray instance on property to avoid object creation in every onDrawOver()
     private val adapterPositionToViews = SparseArray<View>()
-    private val sessionTimeByAdapterPosition = SparseArray<String>()
+    private val cachedDateTimeText = HashMap<DateTime, String>()
 
     private data class TimeText(
         val text: String,
@@ -168,14 +168,11 @@ class SessionsItemDecoration(
             return null
         }
 
-        return if (sessionTimeByAdapterPosition.contains(position)) {
-            sessionTimeByAdapterPosition.get(position)
-        } else {
-            val item = groupAdapter.getItem(position) as? SessionItem ?: return null
-            item.session.startTime.plus(displayTimezoneOffset.value)
+        val item = groupAdapter.getItem(position) as? SessionItem ?: return null
+        return cachedDateTimeText[item.session.startTime]
+            ?: item.session.startTime.plus(displayTimezoneOffset.value)
                 .toString("HH:mm").also {
-                    sessionTimeByAdapterPosition.put(position, it)
+                    cachedDateTimeText[item.session.startTime] = it
                 }
-        }
     }
 }
