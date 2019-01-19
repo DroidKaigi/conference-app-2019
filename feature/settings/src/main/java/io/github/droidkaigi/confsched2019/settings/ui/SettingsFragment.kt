690e0e67da0b52e01f5a37eda5caabb770d341bb
Gen <archiherewego@gmail.com>
2019-01-19 17:37:46 +0900

    Fix looks like flickering when navigate to session page
    set white color to root layout's background
    タイムラインに遷移するときに一瞬画面が真っ赤になる現象の発生メカニズムは以下と推測
    - navigate時に遷移先と遷移元が同時に表示される
    - タイムラインはまずSessionPagesFragmentが表示される
    - session_pages_fragmentの背景色は赤
    - 一方他のレイアウトについては背景色が設定されていない=透明
    - そのため遷移先であるsession_pages_fragmentの赤が全面に透けて見えてしまう
    この問題を解決するため、他のページについてすべて背景色を白に設定した

--- feature/settings/src/main/java/io/github/droidkaigi/confsched2019/settings/ui/SettingsFragment.kt
+++ feature/settings/src/main/java/io/github/droidkaigi/confsched2019/settings/ui/SettingsFragment.kt
@@ -1,10 +1,24 @@
 package io.github.droidkaigi.confsched2019.settings.ui
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.preference.PreferenceFragmentCompat
 import io.github.droidkaigi.confsched2019.settings.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = super.onCreateView(inflater, container, savedInstanceState)
+        root!!.setBackgroundColor(Color.WHITE)
+        return root
+    }
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
     }
