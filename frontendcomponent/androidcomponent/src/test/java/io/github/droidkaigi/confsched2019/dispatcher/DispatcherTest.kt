bc385d6fc609c3821f19e4374a578670442b482a
Takafumi Kanda <kanda.t.web@gmail.com>
2019-01-19 13:42:35 +0530

    Add tests for Dispatcher.launchAndDispatch()

--- frontendcomponent/androidcomponent/src/test/java/io/github/droidkaigi/confsched2019/dispatcher/DispatcherTest.kt
+++ frontendcomponent/androidcomponent/src/test/java/io/github/droidkaigi/confsched2019/dispatcher/DispatcherTest.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.channels.toList
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
 import org.hamcrest.CoreMatchers.hasItems
 import org.hamcrest.core.Is.`is`
 import org.junit.Assert.assertThat
@@ -23,7 +24,7 @@ class DispatcherTest {
         CoroutinePlugin.mainDispatcherHandler = { pseudoMainDispatcher }
     }
 
-    @Test fun sendAndReceive() {
+    @Test fun sendAndReceive_dispatch() {
         val sessionContents: SessionContents = mockk()
         val dispatcher = Dispatcher()
 
@@ -39,7 +40,21 @@ class DispatcherTest {
         }
     }
 
-    @Test fun sendAndMultipleReceive() {
+    @Test fun sendAndReceive_launchAndDispatch() {
+        val sessionContents: SessionContents = mockk()
+        val dispatcher = Dispatcher()
+
+        runBlocking {
+            val allSessionLoaded = async {
+                dispatcher.subscribe<Action.SessionContentsLoaded>().receive()
+            }
+            yield() // Make sure subscribe() is called before launchAndDispatch()
+            dispatcher.launchAndDispatch(Action.SessionContentsLoaded(sessionContents))
+            assertThat(allSessionLoaded.await().sessionContents, `is`(sessionContents))
+        }
+    }
+
+    @Test fun sendAndMultipleReceive_dispatch() {
         val sessionContents: SessionContents = mockk()
         val dispatcher = Dispatcher()
 
@@ -59,7 +74,25 @@ class DispatcherTest {
         }
     }
 
-    @Test fun multipleSendAndReceive() {
+    @Test fun sendAndMultipleReceive_launchAndDispatch() {
+        val sessionContents: SessionContents = mockk()
+        val dispatcher = Dispatcher()
+
+        runBlocking {
+            val allSessionLoaded1 = async {
+                dispatcher.subscribe<Action.SessionContentsLoaded>().receive()
+            }
+            val allSessionLoaded2 = async {
+                dispatcher.subscribe<Action.SessionContentsLoaded>().receive()
+            }
+            yield() // Make sure subscribe() is called before launchAndDispatch()
+            dispatcher.launchAndDispatch(Action.SessionContentsLoaded(sessionContents))
+            assertThat(allSessionLoaded1.await().sessionContents, `is`(sessionContents))
+            assertThat(allSessionLoaded2.await().sessionContents, `is`(sessionContents))
+        }
+    }
+
+    @Test fun multipleSendAndReceive_dispatch() {
         val sessionContents1: SessionContents = mockk()
         val sessionContents2: SessionContents = mockk()
         val dispatcher = Dispatcher()
@@ -81,7 +114,26 @@ class DispatcherTest {
         }
     }
 
-    @Test fun multipleSendAndMultipleReceive() {
+    @Test fun multipleSendAndReceive_launchAndDispatch() {
+        val sessionContents1: SessionContents = mockk()
+        val sessionContents2: SessionContents = mockk()
+        val dispatcher = Dispatcher()
+
+        runBlocking {
+            val allSessionLoaded1 = async {
+                dispatcher.subscribe<Action.SessionContentsLoaded>().take(2).toList()
+            }
+            yield() // Make sure subscribe() is called before launchAndDispatch()
+            dispatcher.launchAndDispatch(Action.SessionContentsLoaded(sessionContents1))
+            dispatcher.launchAndDispatch(Action.SessionContentsLoaded(sessionContents2))
+            assertThat(
+                allSessionLoaded1.await().map { it.sessionContents },
+                `is`(listOf(sessionContents1, sessionContents2))
+            )
+        }
+    }
+
+    @Test fun multipleSendAndMultipleReceive_dispatch() {
         val sessionContents1: SessionContents = mockk()
         val sessionContents2: SessionContents = mockk()
         val dispatcher = Dispatcher()
@@ -103,6 +155,36 @@ class DispatcherTest {
             launch {
                 dispatcher.dispatch(Action.SessionContentsLoaded(sessionContents2))
             }
+            assertThat(
+                allSessionLoaded1.await().map { it.sessionContents },
+                hasItems(sessionContents1, sessionContents2)
+            )
+            assertThat(
+                allSessionLoaded2.await().map { it.sessionContents },
+                hasItems(sessionContents1, sessionContents2)
+            )
+        }
+    }
+
+    @Test fun multipleSendAndMultipleReceive_launchAndDispatch() {
+        val sessionContents1: SessionContents = mockk()
+        val sessionContents2: SessionContents = mockk()
+        val dispatcher = Dispatcher()
+
+        runBlocking {
+            val allSessionLoaded1 = async {
+                dispatcher.subscribe<Action.SessionContentsLoaded>().map {
+                    it
+                }.take(2).toList()
+            }
+            val allSessionLoaded2 = async {
+                dispatcher.subscribe<Action.SessionContentsLoaded>().map {
+                    it
+                }.take(2).toList()
+            }
+            yield() // Make sure subscribe() is called before launchAndDispatch()
+            dispatcher.launchAndDispatch(Action.SessionContentsLoaded(sessionContents1))
+            dispatcher.launchAndDispatch(Action.SessionContentsLoaded(sessionContents2))
             assertThat(
                 allSessionLoaded1.await().map { it.sessionContents },
                 hasItems(sessionContents1, sessionContents2)
