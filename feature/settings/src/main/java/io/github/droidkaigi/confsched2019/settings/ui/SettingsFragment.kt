2da90cd0b63d7f89199b5a7f9a99dd95a7b834d8
Gen <archiherewego@gmail.com>
2019-01-21 16:25:36 +0900

    Revert "Fix looks like flickering when navigate to session page"
    This reverts commit 690e0e6

--- feature/settings/src/main/java/io/github/droidkaigi/confsched2019/settings/ui/SettingsFragment.kt
+++ feature/settings/src/main/java/io/github/droidkaigi/confsched2019/settings/ui/SettingsFragment.kt
@@ -1,24 +1,10 @@
 package io.github.droidkaigi.confsched2019.settings.ui
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.preference.PreferenceFragmentCompat
 import io.github.droidkaigi.confsched2019.settings.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val root = super.onCreateView(inflater, container, savedInstanceState)
-        root!!.setBackgroundColor(Color.WHITE)
-        return root
-    }
-
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
     }
