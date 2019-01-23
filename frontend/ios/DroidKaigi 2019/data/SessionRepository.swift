3af9b227a003b1a396a23187be3b0f0380958f35
nukka123 <nukamiso.master@gmail.com>
2019-01-23 23:13:47 +0900

    Introduce Deferred for coroutines iOS API. Fix network error handing issue.

--- frontend/ios/DroidKaigi 2019/data/SessionRepository.swift
+++ frontend/ios/DroidKaigi 2019/data/SessionRepository.swift
@@ -11,17 +11,71 @@ import RxSwift
 import RxCocoa
 
 final class SessionRepository {
-    
+
     func fetch() -> Single<SessionContents> {
-        return Single<SessionContents>.create { observer -> Disposable in
-            ApiComponentKt.generateDroidKaigiApi().getSessions(callback: { response in
-                observer(.success(ResponseToModelMapperKt.toModel(response)))
-                return KotlinUnit()
-            }, onError: { error in
-                observer(.error(NSError(domain: error.message ?? "No message", code: -1, userInfo: nil)))
-                return KotlinUnit()
-            })
-            return Disposables.create()
+        return ApiComponentKt.generateDroidKaigiApi()
+            .getSessionsAsync()
+            .asSingle(Response.self)
+            .map { ResponseToModelMapperKt.toModel($0) }
+            .catchError { throw handledKotlinException($0) }
+            // .do(onError: { NSLog("fetch error: \($0)") })
+    }
+
+}
+
+
+//FIXME: If you have a more better idea, we can refine this.
+private func handledKotlinException(_ error: Error) -> Error {
+
+    guard let cause = error as? KotlinThrowable else {
+        fatalError("Unexpedeted Eroor: \(error)")
+    }
+
+    // Obtain `NSError` from Ktor `IosHttpRequestException`.
+    // See also: data/api/ThrowableExt.kt
+    if let origin = cause.originNSError {
+        return origin
+    }
+
+    // Return handled kotlin exception.
+    if let handledException = cause as? KotlinException {
+        return handledException
+    }
+
+    fatalError("Unexpedeted KotlinThrowable: \(cause)")
+}
+
+
+// MARK: - Extensions for Kotlin Deferred with RxSwift
+
+extension KotlinThrowable: LocalizedError {
+    public var errorDescription: String? {
+        return self.message ?? "No message. \(self)"
+    }
+}
+
+
+extension Kotlinx_coroutines_core_nativeDeferred {
+    
+    func asSingle<ElementType>(_ elementType: ElementType.Type) -> Single<ElementType> {
+        return Single<ElementType>.create { observer in
+            self.invokeOnCompletion { cause in
+                if let cause = cause {
+                    observer(.error(cause))
+                    return KotlinUnit()
+                }
+
+                if let result = self.getCompleted() as? ElementType {
+                    observer(.success(result))
+                    return KotlinUnit()
+                }
+
+                fatalError("Illegal state or invalid elementType.")
+            }
+
+            return Disposables.create {
+                self.cancel()
+            }
         }
     }
 }
