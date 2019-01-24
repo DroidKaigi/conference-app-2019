c8fdf7de2bc470a63c725b5f719bd045b6547717
MoyuruAizawa <lvla0805@gmail.com>
2019-01-24 15:33:11 +0900

    impl parentLeft parentTop parentRight parentBottom

--- feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/widget/TimeTableLayoutManager.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/widget/TimeTableLayoutManager.kt
@@ -1,8 +1,15 @@
 package io.github.droidkaigi.confsched2019.session.ui.widget
 
+import android.util.SparseArray
 import androidx.recyclerview.widget.RecyclerView
 
 class TimeTableLayoutManager: RecyclerView.LayoutManager() {
+
+    private val parentLeft get() = paddingLeft
+    private val parentTop get() = paddingTop
+    private val parentRight get() = width - paddingRight
+    private val parentBottom get() = height - paddingBottom
+
     override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams {
         return RecyclerView.LayoutParams(
             RecyclerView.LayoutParams.WRAP_CONTENT,
