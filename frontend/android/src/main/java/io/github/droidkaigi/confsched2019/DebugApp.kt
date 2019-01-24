717fcbf962cca9eae4a49c6641dd9e10a74335a1
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-01-24 16:51:17 +0900

    Addressed ktlint

--- frontend/android/src/main/java/io/github/droidkaigi/confsched2019/DebugApp.kt
+++ frontend/android/src/main/java/io/github/droidkaigi/confsched2019/DebugApp.kt
@@ -2,8 +2,6 @@ package io.github.droidkaigi.confsched2019
 
 import com.facebook.stetho.Stetho
 import com.squareup.leakcanary.LeakCanary
-import timber.log.LogcatTree
-import timber.log.Timber
 
 class DebugApp : App() {
     override fun onCreate() {
