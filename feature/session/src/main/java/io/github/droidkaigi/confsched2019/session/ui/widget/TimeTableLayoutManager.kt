c4514fa871ea5b82023b4d2bb239eaf4e0b67406
MoyuruAizawa <lvla0805@gmail.com>
2019-01-24 15:35:59 +0900

    impl func 'measureChild'

--- feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/widget/TimeTableLayoutManager.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/widget/TimeTableLayoutManager.kt
@@ -1,9 +1,15 @@
 package io.github.droidkaigi.confsched2019.session.ui.widget
 
+import android.graphics.Rect
 import android.util.SparseArray
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class TimeTableLayoutManager(private val periodLookUp: (position: Int) -> PeriodInfo) :
+class TimeTableLayoutManager(
+    private val columnWidth: Int,
+    private val pxPerMinute: Int,
+    private val periodLookUp: (position: Int) -> PeriodInfo
+) :
     RecyclerView.LayoutManager() {
 
     class PeriodInfo(val startEpochMilli: Long, val endEpochMilli: Long, val columnNumber: Int)
@@ -32,6 +38,29 @@ class TimeTableLayoutManager(private val periodLookUp: (position: Int) -> Period
             RecyclerView.LayoutParams.WRAP_CONTENT,
             RecyclerView.LayoutParams.WRAP_CONTENT
         )
+    }
+
+    private fun measureChild(view: View, period: Period) {
+        val lp = view.layoutParams as RecyclerView.LayoutParams
+        lp.width = columnWidth
+        lp.height = period.durationMin * pxPerMinute
+
+        val insets = Rect().apply { calculateItemDecorationsForChild(view, this) }
+        val widthSpec = getChildMeasureSpec(
+            width,
+            widthMode,
+            paddingLeft + paddingRight + insets.left + insets.right,
+            lp.width,
+            true
+        )
+        val heightSpec = getChildMeasureSpec(
+            height,
+            heightMode,
+            paddingTop + paddingBottom + insets.top + insets.bottom,
+            lp.height,
+            true
+        )
+        view.measure(widthSpec, heightSpec)
     }
 
     private fun calculateLayout() {
