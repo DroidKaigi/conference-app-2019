891328021cbb86b0fd655c23765d8fb68eb4afb6
MoyuruAizawa <lvla0805@gmail.com>
2019-01-28 03:34:27 +0900

    remove functions of scrollbar computation

--- feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/widget/TimeTableLayoutManager.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/widget/TimeTableLayoutManager.kt
@@ -131,34 +131,6 @@ class TimeTableLayoutManager(
         return scrollAmount
     }
 
-    override fun computeVerticalScrollRange(state: RecyclerView.State): Int {
-        val top = getChildAt(0)?.let(this::getDecoratedTop) ?: return 0
-        val bottom = getChildAt(childCount - 1)?.let(this::getDecoratedBottom) ?: return 0
-        return bottom - top
-    }
-
-    override fun computeVerticalScrollExtent(state: RecyclerView.State): Int {
-        return height - paddingTop - paddingBottom
-    }
-
-    override fun computeVerticalScrollOffset(state: RecyclerView.State): Int {
-        return -(getChildAt(0)?.let(this::getDecoratedTop) ?: 0)
-    }
-
-    override fun computeHorizontalScrollRange(state: RecyclerView.State): Int {
-        val left = getChildAt(0)?.let(this::getDecoratedLeft) ?: return 0
-        val right = getChildAt(childCount - 1)?.let(this::getDecoratedRight) ?: return 0
-        return right - left
-    }
-
-    override fun computeHorizontalScrollExtent(state: RecyclerView.State): Int {
-        return width - paddingBottom - paddingRight
-    }
-
-    override fun computeHorizontalScrollOffset(state: RecyclerView.State): Int {
-        return -(getChildAt(0)?.let(this::getDecoratedLeft) ?: 0)
-    }
-
     private fun measureChild(view: View, period: Period) {
         val lp = view.layoutParams as RecyclerView.LayoutParams
         lp.width = columnWidth
