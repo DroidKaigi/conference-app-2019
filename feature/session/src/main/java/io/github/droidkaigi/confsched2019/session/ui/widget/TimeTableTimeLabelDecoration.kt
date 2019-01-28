33f73b40674df5532e86051761592468bde29ba9
MoyuruAizawa <lvla0805@gmail.com>
2019-01-29 02:04:19 +0900

    fix ItemDecorations

--- feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/widget/TimeTableTimeLabelDecoration.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/widget/TimeTableTimeLabelDecoration.kt
@@ -6,13 +6,14 @@ import android.graphics.Color
 import android.graphics.DashPathEffect
 import android.graphics.Paint
 import android.graphics.Rect
-import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.core.view.children
 import androidx.recyclerview.widget.RecyclerView
 import com.soywiz.klock.DateFormat
 import com.soywiz.klock.format
 import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import io.github.droidkaigi.confsched2019.model.Session
 import io.github.droidkaigi.confsched2019.session.R
 import io.github.droidkaigi.confsched2019.session.ui.item.TabularServiceSessionItem
 import io.github.droidkaigi.confsched2019.session.ui.item.TabularSpacerItem
@@ -72,36 +73,41 @@ class TimeTableTimeLabelDecoration(
     private val textHeight =
         Rect().apply { textPaint.getTextBounds("00:00", 0, "00:00".length - 1, this) }.height()
 
-    // set padding to parent RecyclerView. room number 0 isn't always at the left
-    // or specify the room at the left
-//    override fun getItemOffsets(
-//        outRect: Rect,
-//        view: View,
-//        parent: RecyclerView,
-//        state: RecyclerView.State
-//    ) {
-//        super.getItemOffsets(outRect, view, parent, state)
-//        val roomNumber =
-//            when (val item = groupAdapter.getItem(parent.getChildAdapterPosition(view))) {
-//                is TabularServiceSessionItem -> item.session.room
-//                is TabularSpeechSessionItem -> item.session.room
-//                is TabularSpacerItem -> item.room
-//                else -> return
-//            }.sequentialNumber
-//
-//        if (roomNumber == 0) outRect.left = labelWidth.toInt()
-//    }
-
-    // caches for horizontal dividers and time labels
-    private var referenceView: View? = null
-    private var referenceStart: Long? = null
-    private var sessionStartUnixMilliList: List<Long>? = null
-
     override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
         super.onDraw(c, parent, state)
 
-        // draw vertical line
         val left = parent.children.map { it.left }.min()?.toFloat() ?: return
+        drawVerticalLines(parent, c)
+
+        val originView = parent.getChildAt(0)
+        val originStartUnixMillis = groupAdapter.getItem(parent.getChildAdapterPosition(originView))
+            .startUnixMillis ?: return
+
+        findVisibleSessions(parent).forEach {
+            val gapHeight = TimeUnit.MILLISECONDS
+                .toMinutes(it.startTime.unixMillisLong - originStartUnixMillis)
+                .toInt() * pxPerMin
+            drawHorizontalLines(c, parent, left, originView.top + gapHeight)
+        }
+
+        drawTimeLabelBackground(c, parent)
+
+        findVisibleSessions(parent).forEach {
+            val gapHeight = TimeUnit.MILLISECONDS
+                .toMinutes(it.startTime.unixMillisLong - originStartUnixMillis)
+                .toInt() * pxPerMin
+            drawTimeLabel(c, originView.top + gapHeight, it)
+        }
+    }
+
+    private fun drawTimeLabelBackground(c: Canvas, parent: RecyclerView) {
+        c.drawRect(
+            Rect(0, parent.top, labelWidth.toInt(), parent.bottom),
+            backgroundPaint
+        )
+    }
+
+    private fun drawVerticalLines(parent: RecyclerView, c: Canvas) {
         parent.children
             .distinctBy { it.right }
             .forEach {
@@ -113,79 +119,60 @@ class TimeTableTimeLabelDecoration(
                     straightLinePaint
                 )
             }
+    }
 
-        // draw horizontal line
-        referenceView = parent.getChildAt(0)
-        val referenceView = referenceView ?: return
-        referenceStart =
-            when (val item = groupAdapter.getItem(parent.getChildAdapterPosition(referenceView))) {
-                is TabularSpeechSessionItem -> item.session.startTime.unixMillisLong
-                is TabularServiceSessionItem -> item.session.startTime.unixMillisLong
-                is TabularSpacerItem -> item.startUnixMillis
-                else -> return
+    private fun drawHorizontalLines(c: Canvas, parent: RecyclerView, left: Float, top: Int) {
+        c.drawLine(left, top.toFloat(), parent.right.toFloat(), top.toFloat(), dashLinePaint)
+    }
+
+    private fun drawTimeLabel(c: Canvas, top: Int, it: Session) {
+        val timeText = dateFormat.format(it.startTime.unixMillisLong)
+        val rect =
+            Rect(0, top, labelWidth.toInt(), top + textHeight)
+        val baseX = rect.centerX().toFloat() - textPaint.measureText(timeText) / 2f
+        val baseY =
+            rect.centerY() - (textPaint.fontMetrics.ascent +
+                textPaint.fontMetrics.descent) / 2f
+        c.drawText(timeText, baseX, baseY, textPaint)
+    }
+
+    private fun findVisibleSessions(parent: RecyclerView): List<Session> {
+        val minStart = parent.children.minBy { it.top }
+            ?.let { groupAdapter.getItem(parent.getChildAdapterPosition(it)).startUnixMillis }
+            ?: return emptyList()
+        val maxEnd = parent.children.maxBy { it.bottom }
+            ?.let { groupAdapter.getItem(parent.getChildAdapterPosition(it)).endUnixMillis }
+            ?: return emptyList()
+
+        return (0 until groupAdapter.itemCount)
+            .mapNotNull {
+                val session = when (val item = groupAdapter.getItem(it)) {
+                    is TabularSpeechSessionItem -> item.session as Session
+                    is TabularServiceSessionItem -> item.session as Session
+                    else -> return@mapNotNull null
+                }
+                if (session.startTime.unixMillisLong in minStart..maxEnd) session else null
             }
-        val childrenStart = parent.children.mapNotNull {
-            when (val item = groupAdapter.getItem(parent.getChildAdapterPosition(it))) {
-                is TabularSpeechSessionItem -> item.session.startTime.unixMillisLong
-                is TabularServiceSessionItem -> item.session.startTime.unixMillisLong
-                is TabularSpacerItem -> item.startUnixMillis
+            .distinct()
+    }
+
+    private inline val Item<*>.startUnixMillis: Long?
+        get() {
+            return when (this) {
+                is TabularSpeechSessionItem -> session.startTime.unixMillisLong
+                is TabularServiceSessionItem -> session.startTime.unixMillisLong
+                is TabularSpacerItem -> startUnixMillis
                 else -> null
             }
         }
-        val minStart = childrenStart.min() ?: return
-        val maxEnd = childrenStart.max() ?: return
-        sessionStartUnixMilliList = (0 until groupAdapter.itemCount)
-            .mapNotNull {
-                val startEpochMillis = when (val item = groupAdapter.getItem(it)) {
-                    is TabularSpeechSessionItem -> item.session.startTime.unixMillisLong
-                    is TabularServiceSessionItem -> item.session.startTime.unixMillisLong
-                    else -> return@mapNotNull null
-                }
-                if (startEpochMillis in minStart..maxEnd) startEpochMillis else null
+
+    private inline val Item<*>.endUnixMillis: Long?
+        get() {
+            return when (this) {
+                is TabularSpeechSessionItem -> session.endTime.unixMillisLong
+                is TabularServiceSessionItem -> session.endTime.unixMillisLong
+                is TabularSpacerItem -> endUnixMillis
+                else -> null
             }
-            .distinct()
-
-        val referenceStart = referenceStart ?: return
-        sessionStartUnixMilliList?.forEach {
-            val gapHeight =
-                TimeUnit.MILLISECONDS.toMinutes(it - referenceStart)
-                    .toInt() * pxPerMin
-            val top = referenceView.top + gapHeight
-            c.drawLine(
-                left,
-                top.toFloat(),
-                parent.right.toFloat(),
-                top.toFloat(),
-                dashLinePaint
-            )
         }
-    }
-
-    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-        super.onDrawOver(c, parent, state)
-
-        // draw background
-        c.drawRect(
-            Rect(0, parent.top, labelWidth.toInt(), parent.bottom),
-            backgroundPaint
-        )
-
-        // draw time
-        val referenceView = referenceView ?: return
-        val referenceStart = referenceStart ?: return
-        sessionStartUnixMilliList?.forEach {
-            val gapHeight =
-                TimeUnit.MILLISECONDS.toMinutes(it - referenceStart)
-                    .toInt() * pxPerMin
-            val timeText = dateFormat.format(it)
-            val top = referenceView.top + gapHeight
-            val rect =
-                Rect(0, top, labelWidth.toInt(), top + textHeight)
-            val baseX = rect.centerX().toFloat() - textPaint.measureText(timeText) / 2f
-            val baseY =
-                rect.centerY() - (textPaint.fontMetrics.ascent +
-                    textPaint.fontMetrics.descent) / 2f
-            c.drawText(timeText, baseX, baseY, textPaint)
-        }
-    }
 }
