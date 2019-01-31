a13d28963283cb84e4e1ac2e947524d63197e779
nukka123 <nukamiso.master@gmail.com>
2019-01-31 22:19:53 +0900

    add calendar insert feature.

--- feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/SessionDetailFragment.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/SessionDetailFragment.kt
@@ -111,6 +111,17 @@ class SessionDetailFragment : DaggerFragment() {
                         }
                     )
                 }
+                R.id.session_calendar -> {
+                    val session = binding.session ?: return@setOnMenuItemClickListener false
+                    if (session is SpeechSession) {
+                        activityActionCreator.openCalendar(
+                            session.title.getByLang(defaultLang()),
+                            session.room.name,
+                            session.startTime.unixMillisLong,
+                            session.endTime.unixMillisLong
+                        )
+                    }
+                }
             }
             return@setOnMenuItemClickListener true
         }
