ce81f9e68d94b7e37db89b7093ff05a1af116736
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-02-01 02:24:38 +0900

    Upload artifacts to GH release pages

--- .travis/android/before_install.bash
+++ .travis/android/before_install.bash
@@ -5,6 +5,14 @@ set -eu
 
 source "$(dirname $0)/../bash.source"
 
+mkdir "$HOME/.bin"
+
+curl -sL "https://raw.githubusercontent.com/jmatsu/dpg/master/install.bash" | bash
+curl -sL "https://raw.githubusercontent.com/jmatsu/transart/master/install.bash" | bash
+
+cp ./dpg "$HOME/.bin/"
+cp ./transart "$HOME/.bin/"
+
 unzip release.zip
 
 ./gradlew androidDependenciesExtra getDependencies | grep "Dependencies for" --line-buffered
---
+++ .travis/android/update_github_release.bash
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -eux
+
+create_release_draft() {
+    curl -X POST \
+        -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
+        -d "$(jq -n \
+          --arg tag_name "$RELEASE_TAG_NAME" \
+          --arg target_commitish "$(git rev-parse HEAD)" \
+          --arg name "$RELEASE_TAG_NAME" \
+          --arg body "$(cat .travis/android/release_template)" \
+          '{ "draft": true, "name": $name, "tag_name": $tag_name, "target_commitish": $target_commitish, "body": $body }')" \
+        "https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases"
+}
+
+update_release_draft() {
+    local -r release_id="$1"
+
+    curl -X POST \
+        -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
+        -d "$(jq -n \
+          --arg target_commitish "$(git rev-parse --short HEAD)" \
+          --arg body "$(cat .travis/android/release_template)" \
+          '{ "target_commitish": $target_commitish, "body": $body }')" \
+        "https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases/$release_id"
+}
+
+release_count() {
+    curl -X GET \
+        -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
+        "https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases" | \
+        jq -r '.[] | select(.draft) | length'
+}
+
+get_release_id() {
+    curl -X GET \
+        -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
+        "https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases" | \
+        jq -r --arg tag_name "$RELEASE_TAG_NAME" '.[] | select(.tag_name == $tag_name and .draft) | .id // -1'
+}
+
+ruby -rerb -e 'puts ERB.new(File.read(".travis/android/release_template.travis.erb")).result(binding)' > .travis/android/release_template
+
+release_id="$(get_release_id)"
+
+if let "${release_id:--1} > 0"; then
+    update_release_draft "$release_id"
+else
+    create_release_draft
+fi
