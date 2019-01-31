fc14c9134f5562bf8fc3d22a6dd1fca0c7c7d849
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-01-31 14:49:28 +0900

    Upload apk to deploygate

--- .travis/android/run.bash
+++ .travis/android/run.bash
@@ -4,4 +4,13 @@ set -eu
 
 source "$(dirname $0)/bash.source"
 
-./gradlew assembleRelease --offline
+use_release_keystore
+ ./gradlew bundleRelease --offline
+create_universal_apk_from_aab.bash $(find frontend/android/build/outputs -name "*.aab" | head -1)
+
+curl -sL "https://raw.githubusercontent.com/jmatsu/dpg/master/install.bash" | bash
+
+./dpg apps upload --android \
+  --app-owner droidkaigi
+  --app "$UNIVERSAL_APK_PATH"
+  --message "Release build at $(date)"
