acdfed0b73ec3a697b6fa64acfb655b6d7cd6999
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-02-03 02:25:24 +0900

    Added a preference to register wifi

--- feature/settings/src/main/java/io/github/droidkaigi/confsched2019/settings/ui/SettingsFragment.kt
+++ feature/settings/src/main/java/io/github/droidkaigi/confsched2019/settings/ui/SettingsFragment.kt
@@ -7,5 +7,12 @@ import io.github.droidkaigi.confsched2019.settings.R
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        requireNotNull(findPreference(getString(R.string.pref_key_add_wifi)))
+            .setOnPreferenceClickListener {
+                true.also {
+
+                }
+            }
     }
 }
