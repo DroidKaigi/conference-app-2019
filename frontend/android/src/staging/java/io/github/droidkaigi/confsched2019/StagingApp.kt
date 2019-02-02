fcaf0f0c4389d4d8817acc7a32e4556285bba407
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-02-03 00:20:53 +0900

    Creatae StagingApp class for staging build type

---
+++ frontend/android/src/staging/java/io/github/droidkaigi/confsched2019/StagingApp.kt
@@ -0,0 +1,28 @@
+package io.github.droidkaigi.confsched2019
+
+import com.facebook.stetho.Stetho
+import com.squareup.leakcanary.LeakCanary
+
+class StagingApp : App() {
+    override fun onCreate() {
+        super.onCreate()
+        setupLeakCanary()
+        setupStetho()
+    }
+
+    private fun setupLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+        LeakCanary.install(this)
+    }
+
+    private fun setupStetho() {
+        Stetho.initializeWithDefaults(this)
+    }
+
+    override fun setupLogHandler() {
+        super.setupLogHandler()
+        enableLogcatLogging()
+    }
+}
