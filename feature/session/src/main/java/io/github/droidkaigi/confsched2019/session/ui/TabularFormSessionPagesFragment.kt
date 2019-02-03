e2a2b8eb8b9b1c21bf17558b06c25b31cab7b580
sekiya9311 <sekiya9311@gmail.com>
2019-02-03 23:01:35 +0900

    The place to create fragment is only onCreate

--- feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/TabularFormSessionPagesFragment.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/TabularFormSessionPagesFragment.kt
@@ -25,7 +25,15 @@ class TabularFormSessionPagesFragment : DaggerFragment() {
 
     private lateinit var binding: FragmentTabularFormSessionPagesBinding
     @Inject lateinit var sessionContentsStore: SessionContentsStore
+    private val days = listOf(SessionPage.pageOfDay(1), SessionPage.pageOfDay(2))
     private var nowTabPosition: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        days.forEach { day ->
+            addTabularSessionFragment(day)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,25 +55,14 @@ class TabularFormSessionPagesFragment : DaggerFragment() {
     }
 
     private fun setupSessionPages() {
-        val days = listOf(SessionPage.pageOfDay(1), SessionPage.pageOfDay(2))
-
         // For a few reasons, we stopped using ViewPager in this fragment.
         // See https://github.com/DroidKaigi/conference-app-2019/issues/622 .
         binding.tabularFormSessionsTabLayout.addOnTabSelectedListener(
             object : TabLayout.OnTabSelectedListener {
                 override fun onTabSelected(tab: TabLayout.Tab) {
-                    if (nowTabPosition == tab.position) {
-                        return
-                    }
                     nowTabPosition = tab.position
-
-                    val day = TabularFormSessionPagesFragmentArgs.Builder()
-                        .setDay(days[tab.position].day)
-                        .build()
-                    val selectedFragment = TabularFormSessionPageFragment.newInstance(day)
-                    childFragmentManager.beginTransaction()
-                        .replace(R.id.tabular_form_sessions_fragment_container, selectedFragment)
-                        .commit()
+                    val day = days[tab.position]
+                    switchShowTabularSessionByDay(day)
                 }
 
                 override fun onTabReselected(tab: TabLayout.Tab?) {}
@@ -73,31 +70,63 @@ class TabularFormSessionPagesFragment : DaggerFragment() {
                 override fun onTabUnselected(tab: TabLayout.Tab?) {}
             })
 
-        days.forEachIndexed { index, day ->
+        days.forEach { day ->
             val newTab = binding.tabularFormSessionsTabLayout.newTab().apply {
                 text = day.title
             }
             binding.tabularFormSessionsTabLayout.addTab(newTab, false)
-
-            if (nowTabPosition != null) {
-                return@forEachIndexed
-            }
-
-            // Select first tab
-            if (index == 0 && !isTodayTheSecondDay()) {
-                newTab.select()
-            }
-
-            // Select second tab
-            if (index == 1 && isTodayTheSecondDay()) {
-                newTab.select()
-            }
         }
 
-        // To make tab display state selected, when back to tabular session
         nowTabPosition?.let {
+            // To make tab display state selected, when back to tabular session
             binding.tabularFormSessionsTabLayout.getTabAt(it)?.select()
+        } ?: run {
+            val showTabPos = if (isTodayTheSecondDay()) 1 else 0
+            binding.tabularFormSessionsTabLayout.getTabAt(showTabPos)?.select()
         }
+    }
+
+    private fun addTabularSessionFragment(day: SessionPage.Day) {
+        val (id, show) = when (day) {
+            SessionPage.Day1 -> Pair(
+                R.id.tabular_form_sessions_fragment_container_day1,
+                !isTodayTheSecondDay()
+            )
+            SessionPage.Day2 -> Pair(
+                R.id.tabular_form_sessions_fragment_container_day2,
+                isTodayTheSecondDay()
+            )
+            else -> return
+        }
+        val args = TabularFormSessionPagesFragmentArgs.Builder()
+            .setDay(day.day)
+            .build()
+        val fragment = TabularFormSessionPageFragment.newInstance(args)
+        val trans = childFragmentManager.beginTransaction().replace(id, fragment)
+        if (!show) {
+            trans.hide(fragment)
+        }
+        trans.commit()
+    }
+
+    private fun switchShowTabularSessionByDay(selectedDay: SessionPage.Day) {
+        val (showId, hideId) = when (selectedDay) {
+            SessionPage.Day1 -> Pair(
+                R.id.tabular_form_sessions_fragment_container_day1,
+                R.id.tabular_form_sessions_fragment_container_day2
+            )
+            SessionPage.Day2 -> Pair(
+                R.id.tabular_form_sessions_fragment_container_day2,
+                R.id.tabular_form_sessions_fragment_container_day1
+            )
+            else -> return
+        }
+        val showFragment = childFragmentManager.findFragmentById(showId) ?: return
+        val hideFragment = childFragmentManager.findFragmentById(hideId) ?: return
+        childFragmentManager.beginTransaction()
+            .show(showFragment)
+            .hide(hideFragment)
+            .commit()
     }
 
     private fun isTodayTheSecondDay(): Boolean {
