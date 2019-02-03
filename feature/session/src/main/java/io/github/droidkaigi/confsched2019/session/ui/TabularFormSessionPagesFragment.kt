50b863eea1e4c6531deefdaf2509cd702d7adc6c
sekiya9311 <sekiya9311@gmail.com>
2019-02-03 15:27:38 +0900

    Fix when back to tabular session, the scroll progress is reset

--- feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/TabularFormSessionPagesFragment.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/TabularFormSessionPagesFragment.kt
@@ -25,6 +25,7 @@ class TabularFormSessionPagesFragment : DaggerFragment() {
 
     private lateinit var binding: FragmentTabularFormSessionPagesBinding
     @Inject lateinit var sessionContentsStore: SessionContentsStore
+    private var nowTabPosition: Int? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -53,6 +54,11 @@ class TabularFormSessionPagesFragment : DaggerFragment() {
         binding.tabularFormSessionsTabLayout.addOnTabSelectedListener(
             object : TabLayout.OnTabSelectedListener {
                 override fun onTabSelected(tab: TabLayout.Tab) {
+                    if (nowTabPosition == tab.position) {
+                        return
+                    }
+                    nowTabPosition = tab.position
+
                     val day = TabularFormSessionPagesFragmentArgs.Builder()
                         .setDay(days[tab.position].day)
                         .build()
@@ -71,21 +77,32 @@ class TabularFormSessionPagesFragment : DaggerFragment() {
             val newTab = binding.tabularFormSessionsTabLayout.newTab().apply {
                 text = day.title
             }
-            binding.tabularFormSessionsTabLayout.addTab(newTab)
+            binding.tabularFormSessionsTabLayout.addTab(newTab, false)
+
+            if (nowTabPosition != null) {
+                return@forEachIndexed
+            }
 
             // Select first tab
-            if (index == 0) {
+            if (index == 0 && !isTodayTheSecondDay()) {
                 newTab.select()
             }
 
             // Select second tab
-            if (index == 1) {
-                val jstNow = DateTime.now().toOffset(9.hours)
-                if (jstNow.yearInt == 2019 && jstNow.month1 == 2 && jstNow.dayOfMonth == 8) {
-                    newTab.select()
-                }
+            if (index == 1 && isTodayTheSecondDay()) {
+                newTab.select()
             }
         }
+
+        // To make tab display state selected, when back to tabular session
+        nowTabPosition?.let {
+            binding.tabularFormSessionsTabLayout.getTabAt(it)?.select()
+        }
+    }
+
+    private fun isTodayTheSecondDay(): Boolean {
+        val jstNow = DateTime.now().toOffset(9.hours)
+        return jstNow.yearInt == 2019 && jstNow.month1 == 2 && jstNow.dayOfMonth == 8
     }
 }
 
