ba67aeb8766b3f608e6ca4bef10d20279acc5602
MihoSasaki <miho.sprit@gmail.com>
2019-02-04 12:48:30 +0900

    add files needed for announcement api implementation

---
+++ frontend/ios/DroidKaigi 2019/Scenes/Announcement/AnnouncementsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AnnouncementsViewModel.swift
+//  DroidKaigi 2019
+//
+//  Created by 佐々木美穂 on 2019/02/03.
+//
+
+import Foundation
---
+++ frontend/ios/DroidKaigi 2019/data/ApiHandleRepository.swift
@@ -0,0 +1,8 @@
+//
+//  ApiHandleRepository.swift
+//  DroidKaigi 2019
+//
+//  Created by 佐々木美穂 on 2019/02/03.
+//
+
+import Foundation
