5ce40eedc1bb3caef3c9ebaedaf683276597b96d
Jumpei Matsuda <j.matsuda.bb26th@gmail.com>
2019-02-06 17:58:30 +0900

    See travis tag

--- .travis/android/release_to_alpha.bash
+++ .travis/android/release_to_alpha.bash
@@ -2,6 +2,10 @@
 
 set -eu
 
+if [[ -z "${TRAVIS_TAG:-}" ]]; then
+  exit 0
+fi
+
 die() {
   echo "$*" 1>&2
   exit 1
@@ -11,6 +15,8 @@ source "$(dirname $0)/../bash.source"
 
 source .release/bash.source
 
-transart -f .travis/android/to_github.transart.yml download
+if [[ ! -d ".transart" ]]; then
+  transart -f .travis/android/to_github.transart.yml download
+fi
 
 ./gradlew publishAlpha
